3b18287f9f219e57fd531601e68db80314a79e54
alex ricardo rincon silva <alex@localhost.localdomain>
2013-03-07 12:25:00 -0500

    ya me canse de comentar 6

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ mkdir /etc/apache2/modsecurity.d/
 git clone https://github.com/SpiderLabs/owasp-modsecurity-crs.git
 cp owasp-modsecurity-crs/* /etc/apache2/modsecurity.d/ -rv
 cd /etc/apache2/modsecurity.d/
-for f in `ls base_rules/` ; do sudo ln -s /etc/apache2/modsecurity.d/base_rules/$f activated_$
-for f in `ls optional_rules/ | grep comment_spam` ; do sudo ln -s /etc/apache2/modsecurity.d/$
+for f in `ls base_rules/` ; do sudo ln -s /etc/apache2/modsecurity.d/base_rules/$f activated_rules/$f ; done
+for f in `ls optional_rules/ | grep comment_spam` ; do sudo ln -s /etc/apache2/modsecurity.d/optional_rules/$f activated_rules/$f ; done
 ls -lh activated_rules/
 ln /etc/apache2/mods-available/mod_security2.conf /etc/apache2/mods-enabled/ -sv
 sed 's/SecDefaultAction "phase:1,deny,log"/SecDefaultAction "phase:1,pass,log"/' modsecurity_crs_10_setup.conf.example > modsecurity_crs_10_setup.conf 
